310e1e7367b7448cf230275e145d9964a45ae8ad
uche <uchennanwogu992@gmail.com>
2021-05-12 15:25:37 +0100

    commit

---
+++ serviceworker.js
@@ -0,0 +1,38 @@
+var cacheName = 'petstore v-1';
+var cacheFiles = [
+    'index.html',
+    'product.js',
+    'style.css',
+    'petstore.webmanifest',
+    'images/yarn.jpg',
+    'images/cat-litter.jpg',
+    'images/laser-pointer.jpg',
+    'images/cat-house.jpg',
+    'images/icon-store-512.png',
+
+];
+
+self.addEventListener('install', (e)=>{
+    console.log('[service worker] install');
+    e.waitUntil(
+        caches.open(cacheName).then((cache)=>{
+            console.log('[service worker] caching all files');
+            return cache.addAll(cacheFiles);
+        })
+    )
+});
+
+self.addEventListener('fetch', function(e){
+    e.respondWith(
+        caches.match(e.request).then(function (r){
+            return r || fetch (e.request).then(function(response){
+                return caches.open(cacheName).then(function (cache){
+                    cache.put(e.request, response.clone());
+                    return response;
+                })
+                
+            })
+        })
+    )
+});
+
